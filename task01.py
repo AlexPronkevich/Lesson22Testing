404cd0a959e4860a9ba23d645a16a577a12be2a3
AlexPronkevich <aa-kk@yandex.ru>
2023-04-06 20:55:58 +0300

    Первая часть урока 22

---
+++ task01.py
@@ -0,0 +1,32 @@
+# ls = [ 2 3 4 5 6 7]
+
+def check_sort_asc(ls):
+    for index in range(len(ls) - 1):
+        if ls[index] >= ls[index + 1]:
+            return False
+
+    return True
+
+
+def check_sort_desc(ls):
+    for index in range(len(ls) - 1):
+        if ls[index] <= ls[index + 1]:
+            return False
+
+    return True
+
+def check(ls):
+    return check_sort_asc(ls) or check_sort_desc(ls)
+
+
+def test():
+    print(check([1, 2, 3, 4, 5, 6]))
+    print(check([1, 3, 9, 5, 6]))
+    print(check([6, 5, 4, 3, 2]))
+    print(check([1, 2, 3, 3, 5, 6]))
+
+
+
+if __name__ == "__main__":
+    test()
+
---
+++ task03.py
@@ -0,0 +1,16 @@
+def check_all_the_same(ls):
+    first = ls[0]
+
+    for item in ls:
+        if first != item:
+            return False
+
+    return True
+
+def test():
+    print(check_all_the_same([1, 1, 1]))
+    print(check_all_the_same([1, 3, 2]))
+
+
+if __name__ == "__main__":
+    test()
